8b8ce1e6113d48605b0e9eb9599af47a2280529b
dmleblanc <davedleblanc@outlook.com>
2024-08-12 20:18:17 -0400

    Added Tableau Public Integration; Fixed header / footer links

--- scripts.js
+++ scripts.js
@@ -313,3 +313,32 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+// Tableau embed code
+document.addEventListener('DOMContentLoaded', function() {
+    var divElement = document.getElementById('viz1723481853268');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    
+    if (divElement.offsetWidth > 800) {
+        vizElement.style.minWidth = '420px';
+        vizElement.style.maxWidth = '520px';
+        vizElement.style.width = '100%';
+        vizElement.style.minHeight = '287px';
+        vizElement.style.maxHeight = '387px';
+        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+    } else if (divElement.offsetWidth > 500) {
+        vizElement.style.minWidth = '420px';
+        vizElement.style.maxWidth = '520px';
+        vizElement.style.width = '100%';
+        vizElement.style.minHeight = '287px';
+        vizElement.style.maxHeight = '387px';
+        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+    } else {
+        vizElement.style.width = '100%';
+        vizElement.style.height = '727px';
+    }
+    
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+});
